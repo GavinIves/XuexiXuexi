ab134cffdcf9e13b034c1d09a835b07068717e9a
IVES <38970877+GavinIves@users.noreply.github.com>
2020-04-16 12:42:31 +0800

    Add files via upload

--- autoStudy.bat
+++ autoStudy.bat
@@ -1,4 +1,11 @@
+@echo off
+chcp 65001 
+cd C:\Program Files (x86)\MuMu\emulator\nemu\EmulatorShell
+start NemuPlayer.exe
+timeout /T 15
+cd 填写adb目录
 adb kill-server
 adb connect 127.0.0.1:7555
-cd C:\xuexiqiangguo
+cd 强国文件位置填写处
 start study.py
+pause
