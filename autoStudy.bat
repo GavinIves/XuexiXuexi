abfca2d48f2c94a8c7ce7fa3c9d09825ce94afbd
IVES <38970877+GavinIves@users.noreply.github.com>
2020-08-15 12:52:28 +0800

    Update autoStudy.bat

--- autoStudy.bat
+++ autoStudy.bat
@@ -2,7 +2,7 @@
 chcp 65001 
 cd C:\Program Files (x86)\MuMu\emulator\nemu\EmulatorShell
 start NemuPlayer.exe
-timeout /T 25
+timeout /T 25 (电脑快的可以把时间改小点 慢的改大点）
 cd 填写adb目录
 adb kill-server
 adb connect 127.0.0.1:7555
